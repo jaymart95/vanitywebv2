6080467bcfef40e8ceb1ac5be105fdb789cdf53e
Jason Martin <45865838+jaymart95@users.noreply.github.com>
2023-08-01 17:29:09 -0400

    .

--- dockerfile
+++ dockerfile
@@ -1,2 +1,12 @@
-FROM nginx:alpine
-COPY . /usr/share/nginx/html
+FROM python:3.10.8
+
+RUN apt-get update
+WORKDIR /usr/app
+
+COPY  requirements.txt .
+
+RUN pip install -r requirements.txt
+
+COPY src/ .
+EXPOSE 4545
+CMD [ "python", "app.py" ]
---
+++ src/app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def index():
+    return templates.TemplateResponse("index.html")
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=4545, workers=1)
