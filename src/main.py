048ca20484033f00f4967228b3dd7f6773a92f6f
Jason Martin <45865838+jaymart95@users.noreply.github.com>
2024-06-01 22:15:11 -0400

    .

--- src/main.py
+++ src/main.py
@@ -12,4 +12,4 @@ async def root():
     return FileResponse("app/static/index.html")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=4545)
