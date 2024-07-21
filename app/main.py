6d74424cb7014b376deba5474553117c280c4f11
Jason Martin <45865838+jaymart95@users.noreply.github.com>
2024-07-20 23:42:09 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,15 +5,15 @@ import uvicorn
 
 app = FastAPI(debug=True)
 
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+app.mount("/static", StaticFiles(directory="/usr/app/static"), name="static")
 
 @app.get("/")
 async def root():
-    return FileResponse("app/static/index.html")
+    return FileResponse("/usr/app/static/index.html")
 
 @app.get("/premium")
 async def premium_page():
-    return FileResponse("app/static/premium.html")
+    return FileResponse("/usr/app/static/premium.html")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=4545)
